0c1fd9437f0fb1232b49932e43570ebc4c1a87af
Cameron Gunner <151768650+PYTHONWIZRD@users.noreply.github.com>
2024-02-24 12:27:58 -0500

    Update packet_sniffer.py
    Added the ability to create and save to a pcap file in a directoy.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,15 +1,19 @@
 import scapy.all as scapy
 
-def sniff_packets(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_packet)
 
-def process_packet(packet):
-    if packet.haslayer(scapy.IP):
-        source_ip = packet[scapy.IP].src
-        destination_ip = packet[scapy.IP].dst
-        protocol = packet[scapy.IP].proto
-        print(f"[*] {source_ip} -> {destination_ip} Protocol: {protocol}")
+def sniff_packets(interface, output_file):
+    print("[*] Starting packet capture...")
+    packets = scapy.sniff(iface=interface, store=True)
+    print(f"[*] Packet capture completed. Captured {len(packets)} packets.")
+
+    print("[*] Writing captured packets to PCAP file...")
+    scapy.wrpcap(output_file, packets)
+    print(f"[*] Packets saved to {output_file}")
+
 
 interface = input("Enter the interface to sniff on (e.g., eth0): ")
-sniff_packets(interface)
+output_file = input("Enter the name of the output PCAP file: ")
+
+sniff_packets(interface, output_file)
+
 
